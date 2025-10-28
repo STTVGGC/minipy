1e41c9c15f5eb5c411ee8e3ad75b2ee362fa42b0
王子昊 <2531336118@qq.com>
2025-10-28 16:30:18 +0800

    更改脚本，防止json数据丢失

--- .auto_update.sh
+++ .auto_update.sh
@@ -3,6 +3,6 @@ cd /home/ubuntu/minipy || exit
 echo "拉取最新代码..."
 git fetch origin
 git reset --hard origin/main
-git clean -fd
+git clean -fd -e messages.json
 echo "更新完成，重启服务..."
 sudo systemctl restart minipy
