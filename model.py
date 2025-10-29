cd38bd83d73ac52ffcbe0005194d8c1cc150e340
王子昊 <2531336118@qq.com>
2025-10-29 10:51:37 +0800

    创建数据库模型

---
+++ model.py
@@ -0,0 +1,12 @@
+# models.py
+from tortoise import fields
+from tortoise.models import Model
+
+class Message(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=50)
+    content = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}: {self.content[:20]}"
